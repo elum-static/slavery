77b95667fa08073c251d13655eb4bee1b50c72a0
Егор <root@apiteam.ru>
2024-03-17 22:32:25 +0300

    update

---
+++ v1/js/BgNv4_IU.js
@@ -0,0 +1,1 @@
+import{c as a,M as o,b as s}from"./04G9GEk8.js";const e=e=>a(o,{onClick:()=>{s(1)},mode:"panel"});export{e as default};
--- v1/js/CqogI-8H.js
+++
@@ -1,1 +0,0 @@
-import{c as a,M as o,b as s}from"./DtnzoLfw.js";const e=e=>a(o,{onClick:()=>{s(1)},mode:"panel"});export{e as default};
